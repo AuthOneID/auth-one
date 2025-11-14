aeccdac07836c5f4b69a2e77ac0ebd71801f7a2f
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2025-11-14 10:28:13 +0700

    feat: add AppProvider to manage user and group initialization

---
+++ providers/app_provider.ts
@@ -0,0 +1,54 @@
+import Group from '#models/group'
+import User from '#models/user'
+import type { ApplicationService } from '@adonisjs/core/types'
+
+export default class AppProvider {
+  constructor(protected app: ApplicationService) {}
+
+  /**
+   * Register bindings to the container
+   */
+  register() {}
+
+  /**
+   * The container bindings have booted
+   */
+  async boot() {}
+
+  /**
+   * The application has been booted
+   */
+  async start() {}
+
+  /**
+   * The process has been started
+   */
+  async ready() {
+    const groupExist = await Group.query().first()
+    let groupId = groupExist?.id
+    if (!groupExist) {
+      const { id } = await Group.create({
+        name: 'AuthOne Admins',
+        isSuperuser: true,
+      })
+      groupId = id
+    }
+
+    const userExist = await User.query().first()
+    if (!userExist) {
+      const user = await User.create({
+        email: '',
+        fullName: 'AuthOne Admin',
+        password: 'admin',
+        username: 'admin',
+      })
+
+      user.related('groups').attach([groupId!])
+    }
+  }
+
+  /**
+   * Preparing to shutdown the app
+   */
+  async shutdown() {}
+}
