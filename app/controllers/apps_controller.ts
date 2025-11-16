84036d34bb2f8671e44cdfefdc003226a969ffac
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2025-11-16 07:31:07 +0700

    feat: implement app filtering logic based on user roles and groups in AppsController

--- app/controllers/apps_controller.ts
+++ app/controllers/apps_controller.ts
@@ -1,12 +1,34 @@
+import Application from '#models/application'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AppsController {
   public async index({ inertia, auth }: HttpContext) {
     await auth.user?.load('groups')
+    await auth.user?.load('roles')
+    await auth.user?.load('applications')
     const isSuperAdmin = auth.user?.groups.some((g) => g.isSuperuser)
 
+    const apps = await Application.query().preload('users').preload('groups').preload('roles')
+
+    // Filter apps based on user access
+    const filteredApps = apps.filter((app) => {
+      const appUserIds = app.users.map((u) => u.id)
+      const appGroupIds = app.groups.map((g) => g.id)
+      const appRoleIds = app.roles.map((r) => r.id)
+
+      const userGroupIds = auth.user!.groups.map((g) => g.id)
+      const userRoleIds = auth.user!.roles.map((r) => r.id)
+
+      const isAllowed =
+        appUserIds.includes(auth.user!.id) ||
+        appGroupIds.some((id) => userGroupIds.includes(id)) ||
+        appRoleIds.some((id) => userRoleIds.includes(id))
+
+      return isAllowed || isSuperAdmin
+    })
+
     return inertia.render('apps/index', {
-      apps: [],
+      apps: filteredApps,
       isSuperAdmin,
     })
   }
