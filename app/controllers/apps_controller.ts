2128687a7b4f8ede0d3f85c00b7ad3c67aaf9b6f
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2025-11-17 08:11:24 +0700

    fix: ensure appUrl redirection only occurs for non-superadmin users with a single filtered app

--- app/controllers/apps_controller.ts
+++ app/controllers/apps_controller.ts
@@ -27,13 +27,8 @@ export default class AppsController {
       return isAllowed || isSuperAdmin
     })
 
-    if (filteredApps.length === 1 && filteredApps[0].appUrl) {
+    if (!isSuperAdmin && filteredApps.length === 1 && filteredApps[0].appUrl) {
       return inertia.location(filteredApps[0].appUrl)
     }
-
-    return inertia.render('apps/index', {
-      apps: filteredApps,
-      isSuperAdmin,
-    })
   }
 }
