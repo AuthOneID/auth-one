d34709af327894d2e6e86e96065b2c77d351c498
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2025-11-20 09:04:24 +0700

    feat: update Header component to use dynamic logo from settings

--- inertia/components/layout/header.tsx
+++ inertia/components/layout/header.tsx
@@ -1,15 +1,20 @@
-import { Link } from '@inertiajs/react'
+import { Link, usePage } from '@inertiajs/react'
 import { SidebarTrigger } from './sidebar'
 import { DropdownUser } from './user_dropdown'
+import { SharedProps } from '@adonisjs/inertia/types'
 
 export const Header = ({ initial }: { initial: string }) => {
+  const {
+    props: { settings },
+  } = usePage<SharedProps & Record<string, unknown>>()
+
   return (
     <header className="bg-background flex justify-between md:justify-end h-14 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 fixed top-0 left-0 right-0 w-full z-10">
       <div className="flex items-center gap-2 px-4 md:hidden">
         <SidebarTrigger className="-ml-1 cursor-pointer" />
       </div>
       <Link href="/" className="hover:bg-gray-100 rounded-md py-3 block md:hidden">
-        <img src="/img/logo.png" className="h-6 mx-auto" alt="logo" />
+        <img src={settings.logo || '/img/logo.png'} className="h-6 mx-auto" alt="logo" />
       </Link>
       <div className="px-5 flex items-center gap-2.5">
         <DropdownUser initial={initial}></DropdownUser>
